c261d4490daf340ff8fee1ec40f600dc8a6b5e74
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-03-15 16:39:07 -0700

    updated DOM but test not passing

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const changeText = document.getElementById('text');
+    changeText.innerText = "This is really cool!";
+  })
